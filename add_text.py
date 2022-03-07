c9afd0f25e0807cd773827800ab843dd54a985b5
dylan-robins <dylan.robins@gmail.com>
2022-03-07 23:12:25 +0100

    Added rivers and improved text rendering

--- add_text.py
+++ add_text.py
@@ -10,15 +10,65 @@ from palette import Palette, load_colour_palettes
 PALETTE_FILE = Path("base16_schemes.json")
 
 
+def add_border(
+    img: Image.Image,
+    fill: str,
+    border_size: int | tuple[int, int] | tuple[int, int, int, int],
+) -> Image.Image:
+    """
+    Adds a border around the provided image
+
+    Parameters
+    ----------
+    img : Image
+        The image to add the border to.
+    fill : str
+        The colour of the border
+    border_size : int | tuple[int, int] | tuple[int, int, int, int]
+        The size of the border, in one of three formats:
+        - int: uniform border around the image
+        - tuple[int, int]: horizontal/vertical border sizes
+        - tuple[int, int, int, int]: N/E/S/W border sizes
+    """
+
+    W, H = img.size
+    if isinstance(border_size, int):
+        desired_size = W + border_size * 2, H + border_size * 2
+
+    elif len(border_size) == 2:
+        hpad, vpad = border_size
+        desired_size = W + vpad * 2, H + hpad * 2
+
+    elif len(border_size) == 4:
+        Npad, Epad, Spad, Wpad = border_size
+        desired_size = W + Wpad + Epad, H + Npad + Spad
+    else:
+        raise ValueError(
+            f"Invalid value {repr(border_size)} provided to argument border_size"
+        )
+
+    print(f"### Resizing image to {desired_size[0]}x{desired_size[1]} (fill: {fill})")
+
+    new_im = Image.new("RGB", desired_size, fill)
+    new_im.paste(
+        img, ((desired_size[0] - W) // 2, (desired_size[1] - H) // 2,),
+    )
+    return new_im
+
+
 def add_city_name(input_image: Path, output_image: Path, palette: Palette):
+
     print(f"### Loading image from {input_image}")
     img = Image.open(input_image)
 
     W, H = img.size
     print(f"### Loaded image of size {W}x{H}")
 
+    # add a small border
+    img = add_border(img, palette["base05"], 10)
+
     # portion of image width you want text width to be
-    img_fraction = (W - 20) / W
+    img_fraction = (W - 100) / W
     fontsize = 72
     text = "GRENOBLE"
     font = ImageFont.truetype("fonts/Raleway.ttf", fontsize)
@@ -35,36 +85,27 @@ def add_city_name(input_image: Path, output_image: Path, palette: Palette):
 
     desired_size = W + 0, H + text_H
     print(f"### Resizing image to {desired_size[0]}x{desired_size[1]}")
-    img = ImageOps.pad(
-        img,
-        desired_size,
-        color=palette["base00"],
-        centering=(0.5, 0),
-    )
+    img = ImageOps.pad(img, desired_size, color=palette["base00"], centering=(0.5, 0),)
 
     W, H = img.size
     x, y = 0, H - text_H - 10
     print(f"### Rendering text at coordinates {x}x{y}")
     draw = ImageDraw.Draw(img)
     draw.text(
-        xy=(x, y),
-        text=text,
-        fill=palette["base05"],
-        font=font,
-        align="center",
+        xy=(x, y), text=text, fill=palette["base05"], font=font, align="center",
     )
 
-    desired_size = ceil(W*1.5), ceil(H*1.5)
-    print(f"### Resizing image to {desired_size[0]}x{desired_size[1]}")
-    img = ImageOps.pad(
-        img,
-        desired_size,
-        color=palette["base02"],
-        centering=(0.5, 0.5),
-    )
+    img = add_border(img, palette["base00"], 250)
 
     print(f"### Exporting image to {output_image}")
     img.save(output_image, optimize=True)
+
+    preview_path = (
+        output_image.parent / f"{output_image.stem}.small{output_image.suffix}"
+    )
+    print(f"### Exporting preview to {preview_path}")
+    thumbnail = ImageOps.contain(img, (1920, 1920))
+    thumbnail.save(preview_path, optimize=True)
 
 
 if __name__ == "__main__":
--- generate_map.py
+++ generate_map.py
@@ -9,77 +9,125 @@ import argparse
 from email.policy import default
 from pathlib import Path
 
+import geopandas as gpd
 import osmnx as ox
+from geopy.geocoders import Nominatim
 
 from palette import Palette, load_colour_palettes
 
 
 class Map:
-    def __init__(self, places) -> None:
+    def __init__(self, point: tuple[float, float], radius: int) -> None:
         print("### Loading data from OpenStreetMap")
-        self.graph = ox.graph_from_place(places)
-        self.data = []
-        self.colours = []
-        self.unpack_data()
+        self.streets = ox.graph_from_point(
+            point,
+            dist=radius,
+            retain_all=True,
+            truncate_by_edge=True,
+            clean_periphery=False,
+        )
+        self.street_data = self.unpack_data()
+        if "geometry" not in self.street_data:
+            raise KeyError("geometries not found in street data!")
 
-    def unpack_data(self):
-        for uu, vv, kkey, ddata in self.graph.edges(keys=True, data=True):
-            self.data.append(ddata)
+        self.water = ox.geometries_from_point(
+            point, tags={"water": "river"}, dist=radius,
+        )
+
+    def unpack_data(self) -> gpd.GeoDataFrame:
+        data = []
+        for _, _, _, ddata in self.streets.edges(keys=True, data=True):
+            data.append(ddata)
+        df = gpd.GeoDataFrame(data)
+        df.to_csv("streets.csv")
+        return df
 
     def apply_colour_palette(self, palette_name: str, palette: Palette):
         print(f"### Applying palette {palette_name} to data")
 
-        # The length is in meters
-        for item in self.data:
-            if "length" in item.keys():
-
-                if item["length"] <= 100:
-                    color = palette["base08"]
-
-                elif item["length"] > 100 and item["length"] <= 200:
-                    color = palette["base09"]
-
-                elif item["length"] > 200 and item["length"] <= 400:
-                    color = palette["base0A"]
-
-                elif item["length"] > 400 and item["length"] <= 800:
-                    color = palette["base0B"]
-
-                else:
-                    color = palette["base05"]
+        def _map_highway_type_to_colour(row: gpd.GeoSeries) -> str:
+            if isinstance(row.highway, str):
+                comparer = lambda way, ref: way in ref
             else:
-                color = palette["base05"]
+                comparer = lambda way, ref: any(i in ref for i in way)
 
-            self.colours.append(color)
+            if comparer(row.highway, ["motorway"]):
+                return palette["base05"]
+            elif comparer(row.highway, ["trunk"]):
+                return palette["base05"]
+            elif comparer(
+                row.highway,
+                ["primary", "secondary", "tertiary", "unclassified", "residential"],
+            ):
+                return palette["base05"]
+            elif comparer(row.highway, ["path"]):
+                return palette["base03"]
+            else:
+                return palette["base02"]
+
+        def _map_highway_type_to_width(row: gpd.GeoSeries) -> int:
+            if isinstance(row.highway, str):
+                comparer = lambda way, ref: way in ref
+            else:
+                comparer = lambda way, ref: any(i in ref for i in way)
+
+            if comparer(row.highway, ["motorway", "trunk"]):
+                return 2
+            else:
+                return 1
+
+        self.street_data["colour"] = self.street_data.apply(
+            _map_highway_type_to_colour, axis=1
+        )
+        self.street_data["line_width"] = self.street_data.apply(
+            _map_highway_type_to_width, axis=1
+        )
+
+    @property
+    def streets_bbox(self) -> tuple[float, float, float, float]:
+        minx, miny, maxx, maxy = self.street_data.geometry.total_bounds
+        return (maxy, miny, maxx, minx)
 
     def export_image(self, destination: Path, palette_name: str, palette: Palette):
         self.apply_colour_palette(palette_name, palette)
 
         print("### Generating image")
-        ox.plot_graph(
-            self.graph,
+        fig, ax = ox.plot_graph(
+            self.streets,
             node_size=0,
-            dpi=300,
-            figsize=(9, 12),
+            figsize=(20, 20),
             bgcolor=palette["base00"],
-            edge_color=self.colours,
+            edge_color=self.street_data["colour"].to_list(),
             edge_linewidth=1,
             edge_alpha=1,
-            filepath=destination,
             show=False,
-            save=True,
+            save=False,
             close=True,
+        )
+
+        ox.plot_footprints(
+            self.water,
+            color=palette["base01"],
+            bbox=self.streets_bbox,
+            ax=ax,
+            dpi=600,
+            save=True,
+            show=False,
+            close=False,
+            filepath=destination,
         )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--places",
+        "--place",
         type=str,
-        nargs="+",
         help="The place to generate the map for",
         default="Grenoble, France",
+    )
+    parser.add_argument(
+        "--radius", type=int, help="Radius around the point to generate", default=3000,
     )
     parser.add_argument(
         "--palette_file",
@@ -102,7 +150,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    map = Map(args.places)
+    geolocator = Nominatim(user_agent="map_plotter")
+    location = geolocator.geocode(args.place)
+
+    map = Map((location.latitude, location.longitude), args.radius,)
 
     args.export_dir.mkdir(exist_ok=True)
 
